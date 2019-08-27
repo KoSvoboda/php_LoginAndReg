117612adf83e603d1b66fdbbda62485105d9e2b3
KoSvoboda <kosvoboda@gmail.com>
2019-08-27 23:38:20 +0300

    v. 1.2.2
    - Delete duplicated and non-used files

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$users = simplexml_load_file('users.xml');
-print_r($users);
-echo $users;
-//$result = $users->xpath('users/'.$username.'/username');
-foreach ($users->children() as $user) {
-    if ($user['username']=='123') echo 'OK';
-}
